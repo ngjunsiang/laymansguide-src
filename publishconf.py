e9a2770a59a76c31d07b67eeb202f27063076229
JS <kureshii@js-xiaomi.localdomain>
2026-06-10 18:52:57 +0800

    fix: change SITEURL after repo move

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://ngjunsiang.github.io/laymansguide-src"
+SITEURL = "https://ngjunsiang.github.io/laymansguide"
 RELATIVE_URLS = False
 
 # CSS path will be resolved relative to SITEURL by the template
